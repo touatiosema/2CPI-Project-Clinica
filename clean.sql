139b6ee62f82d746b2ec5d2e5ac657f099142081
Anis BOUCHEKHIMA <bdzanis.ab@gmail.com>
2018-02-16 19:04:51 +0100

    Cleanup

--- clean.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE exams_in_consultations;
-DROP TABLE exams;
-DROP TABLE meds_in_consultations;
-DROP TABLE meds;
-DROP TABLE consultations;
-DROP TABLE agenda;
-DROP TABLE patients;
-DROP TABLE doctors;
